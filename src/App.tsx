a609da9618dfb38436aded8de4fcf922371f5fc7
Taek Been Nam <taekbeen93@gmail.com>
2023-12-19 15:07:32 -0500

    adjust padding

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,11 @@ function App() {
   return (
     <>
       <div className="fixed top-0 h-[320px] w-full bg-no-repeat bg-cover bg-hero-pattern-mobile sm:bg-hero-pattern-desktop"></div>
-      <section className="relative top-52 flex flex-col mx-8 sm:mx-auto px-10 min-w-0 bg-white drop-shadow-lg max-w-2xl rounded-xl">
-        <header className="flex gap-4 my-10">
+      <section
+        aria-label="FAQs"
+        className="relative top-52 flex flex-col mx-8 sm:mx-auto px-10 min-w-0 bg-white drop-shadow-lg max-w-2xl rounded-xl"
+      >
+        <header aria-label="FAQ header" className="flex gap-4 my-10">
           <IconStar className="w-8 h-18" />
           <h1 className="text-4xl lg:text-5xl font-bold text-purple-950">
             FAQs
